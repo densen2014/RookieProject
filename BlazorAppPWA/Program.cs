74a799afe536a0f7d7aeb5c12f77b77fcb640ebd
alex chow <zhouchuanglin@gmail.com>
2020-07-09 15:55:39 +0200

    嗡嗡嗡

--- BlazorAppPWA/Program.cs
+++ BlazorAppPWA/Program.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Blazored.LocalStorage;
+using System.IO;
+using System.Reflection;
 
 namespace BlazorAppPWA
 {
@@ -15,13 +18,27 @@ namespace BlazorAppPWA
 
         public static async Task Main(string[] args)
         {
+            string fileName = "BlazorAppPWA.appsettings.json";
+            var stream = Assembly.GetExecutingAssembly()
+                                 .GetManifestResourceStream(fileName);
+
+            var config = new ConfigurationBuilder()
+                     .AddJsonStream(stream)
+                     .Build();
+
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
             //builder.Services.AddSingleton(fsql);
             builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+            builder.Services.AddBlazoredLocalStorage();
+            builder.Services.AddTransient(_ =>
+            {
+                return config.GetSection("GalaxyStuff")
+                             .Get<GalaxyInfo>();
+            });
             await builder.Build().RunAsync();
+
         }
     }
 }
