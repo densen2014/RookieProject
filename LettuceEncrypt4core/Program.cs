76cc9214851c2bca4cea548778da1d0a07026d56
Alex Chow <zhouchuanglin@gmail.com>
2021-05-22 21:40:17 +0200

    使用预发行版本  use the pre-release versions from nuget.org

--- LettuceEncrypt4core/Program.cs
+++ LettuceEncrypt4core/Program.cs
@@ -1,13 +1,8 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Threading.Tasks;
-using LettuceEncrypt;
+using Microsoft.AspNetCore.Server.Kestrel.Https;
 
 namespace LettuceEncrypt4core
 {
@@ -22,20 +17,21 @@ namespace LettuceEncrypt4core
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    //webBuilder.UseKestrel(k =>
-                    //{
-                    //    var appServices = k.ApplicationServices;
-                    //    k.Listen(
-                    //        IPAddress.Any, 443,
-                    //        o => o.UseHttps(h =>
-                    //        {
-                    //            h.UseLettuceEncrypt(appServices);
-                    //        }));
-                    //});
                     webBuilder.UseStartup<Startup>();
 
+                    webBuilder.UseKestrel(k =>
+                    {
+                        var appServices = k.ApplicationServices;
+                        k.Listen(
+                            IPAddress.Any, 443,
+                            o => o.UseHttps(h =>
+                            {
+                                h.UseLettuceEncrypt(appServices);
+                            }));
+                    });
+
+
+                    
                 });
-
-
     }
 }
