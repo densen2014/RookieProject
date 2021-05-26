59bda0a6a309aa871b04142c5678ebb32f656eae
Alex Chow <zhouchuanglin@gmail.com>
2021-05-26 20:53:57 +0200

    SignalRChatWPF Works!

--- SignalRChatWPF/App.xaml.cs
+++ SignalRChatWPF/App.xaml.cs
@@ -1,17 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.DependencyInjection;
 using System.Windows;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 
 namespace SignalRChatWPF
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
+
     public partial class App : Application
     {
+        private IHost _host;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            _host = Host.CreateDefaultBuilder(e.Args)
+                .ConfigureWebHostDefaults(webHostBuilder => webHostBuilder.UseStartup<SignalRChat.Startup>())
+                .ConfigureServices(services =>
+                {
+                    services.AddTransient<MainWindow>();
+                })
+                .Build();
+
+            _host.Start();
+
+            _host.Services.GetRequiredService<MainWindow>().Show();
+        }
+
+        protected override void OnExit(ExitEventArgs e) => _host.Dispose();
     }
 }
