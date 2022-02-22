92a78b04959780796706c45e3589c50b499414f3
Alex Chow <zhouchuanglin@gmail.com>
2022-02-22 12:55:41 +0100

    FileStreams461

---
+++ FileStreams461/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace FileStreams461
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.打开文件ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 25);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(818, 684);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.打开文件ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(818, 25);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 打开文件ToolStripMenuItem
+            // 
+            this.打开文件ToolStripMenuItem.Name = "打开文件ToolStripMenuItem";
+            this.打开文件ToolStripMenuItem.Size = new System.Drawing.Size(68, 21);
+            this.打开文件ToolStripMenuItem.Text = "打开文件";
+            this.打开文件ToolStripMenuItem.Click += new System.EventHandler(this.打开文件ToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(818, 709);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 打开文件ToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ FileStreams461/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics; 
+using System.IO;
+
+namespace FileStreams461
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.Add("key", "偏移量");
+            dataGridView1.Columns.Add("hex", "Hex");
+            dataGridView1.Columns.Add("text", "ASCii");
+            dataGridView1.Columns[0].Width = 200;
+            dataGridView1.Columns[1].Width = 700;
+            dataGridView1.Columns[2].Width = 400;
+            openFileDialog1.InitialDirectory = Application.StartupPath;
+            openFileDialog1.FileName = "TextFile1.txt";
+
+            this.Text = "演示只打开最大500,每次16字节";
+        }
+
+        private void 打开文件ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //获取文件路径
+            string filePath = "TextFile1.txt";
+
+            var res = openFileDialog1.ShowDialog();
+            if (res == DialogResult.OK)
+            {
+                filePath = openFileDialog1.FileName;
+            }
+            dataGridView1.Rows.Clear();
+
+            //循环读取大文本文件
+            FileStream fsRead;
+            //用FileStream文件流打开文件
+            try
+            {
+                fsRead = new FileStream(@filePath, FileMode.Open, FileAccess.Read);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            //还没有读取的文件内容长度
+            long leftLength = fsRead.Length > 500 ? 500 : fsRead.Length;
+            //创建接收文件内容的字节数组
+            byte[] buffer = new byte[16];
+            //每次读取的最大字节数
+            int maxLength = buffer.Length;
+            //每次实际返回的字节数长度
+            int num = 0;
+            //文件开始读取的位置
+            int fileStart = 0;
+            while (leftLength > 0)
+            {
+                //设置文件流的读取位置
+                fsRead.Position = fileStart;
+                if (leftLength < maxLength)
+                {
+                    num = fsRead.Read(buffer, 0, Convert.ToInt32(leftLength));
+                }
+                else
+                {
+                    num = fsRead.Read(buffer, 0, maxLength);
+                }
+                if (num == 0)
+                {
+                    break;
+                }
+                fileStart += num;
+                leftLength -= num;
+                Debug.WriteLine(Encoding.Default.GetString(buffer));
+                var split = buffer.ToHex().Split('\t');
+                dataGridView1.Rows.Add(fileStart, buffer.ToHex(), Encoding.Default.GetString(buffer));
+
+            }
+            fsRead.Close();
+
+        }
+    }
+    public static class Tools
+    {
+        /// <summary>
+        /// 转换为16进制
+        /// </summary>
+        /// <param name="bytes"></param>
+        /// <param name="lowerCase">是否小写</param>
+        /// <returns></returns>
+        public static string ToHex(this byte[] bytes, bool lowerCase = false)
+        {
+            if (bytes == null)
+                return null;
+
+            var result = new StringBuilder();
+            var format = lowerCase ? "x2" : "X2";
+            int j = 0;
+            for (var i = 0; i < bytes.Length; i++)
+            {
+                j++;
+                result.Append(bytes[i].ToString(format) + (j == 4 ? "   " : " "));
+                j = j == 4 ? 0 : j;
+            }
+
+            return result.ToString();
+        }
+
+    }
+
+}
