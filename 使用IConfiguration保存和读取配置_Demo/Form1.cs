3e9888bcba5eb3bf1b7e5884d8fb9a53d589fa96
Alex Chow <zhouchuanglin@gmail.com>
2022-03-21 04:25:21 +0100

    添加使用IConfiguration保存和读取配置_Demo

---
+++ 使用IConfiguration保存和读取配置_Demo/Form1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace IConfiguration_demo
+{
+    public partial class Form1 : Form
+    {
+        public class MySettings
+        {
+            public string? Text { get; set; }
+            public Color BackColor { get; set; }
+            public Size Size { get; set; }
+        }
+        
+        public Form1()
+        {
+            InitializeComponent();
+            var mySettings = Program.Config!.GetSection("MySettings").Get<MySettings>();
+            this.Text = mySettings.Text;
+            this.BackColor = mySettings.BackColor;
+            this.Size = mySettings.Size;
+
+        }
+    }
+}
---
+++ 使用IConfiguration保存和读取配置_Demo/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace IConfiguration_demo
+{
+    internal  class Program
+    {
+        public static IConfiguration? Config;
+
+
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            Config = builder.Build();
+
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+            //Application.Run(new FormLiveChartsTest());
+
+        }
+    }
+}
