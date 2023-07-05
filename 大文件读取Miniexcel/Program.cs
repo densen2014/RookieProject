aaddc20e8683b386a29163f3f376d5729b45c640
Alex chow <zhouchuanglin@gmail.com>
2023-07-06 00:17:59 +0800

    大文件读取Miniexcel

---
+++ 大文件读取Miniexcel/Program.cs
@@ -0,0 +1,20 @@
+﻿// ********************************** 
+// Densen Informatica 中讯科技 
+// 作者：Alex Chow
+// e-mail:zhouchuanglin@gmail.com 
+// **********************************
+
+using MiniExcelLibs;
+using System.Diagnostics;
+using System.IO;
+
+var sw = new Stopwatch ();
+sw.Start ();
+Console.WriteLine("Hello, World!");
+var row = MiniExcel.Query(@"C:\Users\Alex\Desktop\RawData.csv").Skip(200000).Take(1).ToList();
+
+Console.WriteLine(row[0].A);
+//row.ToList().ForEach(a=> Console.WriteLine(a.A));
+
+sw.Stop();
+Console.WriteLine($"Cost {sw.ElapsedMilliseconds} ms");
